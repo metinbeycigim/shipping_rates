cab3c30f0a1ac2b6e92af118f17311c3346112b3
metinbaydere <metinbaydere@gmail.com>
2022-12-23 13:24:26 -0600

    carrier account codes entered

--- lib/shipstation_credentials.dart
+++ lib/shipstation_credentials.dart
@@ -1,4 +1,7 @@
 class ShipstationCredentials {
   static const String key = 'c4cc7cdbbcca404aaa6025cd66a8cc62';
   static const String secret = 'd8b4cd10d1bf48eb9f243707ef8b63ba';
+  static const int fedexAccountNumber = 378298;
+  static const int upsAccountNumber = 272379;
+  static const int stampsAccountNumber = 93103;
 }
